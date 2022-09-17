49e5c0415875691ecaec3fd56d1c6d9a886d35c7
Maryam Fatima <87557946+Maryam-Mehmood@users.noreply.github.com>
2022-09-17 11:18:24 +0500

    a

--- nav.php
+++ nav.php
@@ -13,7 +13,7 @@
 	     <nav class="navbar navbar-expand-lg navbar-light bg-light">
 		     <div class="container-fluid">
 			     <a class="navbar-brand" href="index.php">
-				     <img src="images/l.png" alt="" width="130px">
+				     <img src="images/logo.png" alt="" width="130px">
 				 </a>
 				 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-host" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
 				     <span class="icon-bar"></span>
