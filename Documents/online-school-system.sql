713439f6a4c4d76a26f3b15734b92c3027d83fe5
Maryam Fatima <87557946+Maryam-Mehmood@users.noreply.github.com>
2022-09-20 11:54:49 +0500

    a

--- Documents/online-school-system.sql
+++ Documents/online-school-system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2022 at 08:51 AM
+-- Generation Time: Sep 20, 2022 at 08:54 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
