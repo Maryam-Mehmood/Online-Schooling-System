34e6fd17f295cf109ec16abcb56e24e55540a1be
Momna Khan <87558364+MomnaKhan@users.noreply.github.com>
2022-09-20 09:37:58 -0700

    paste

--- index.php
+++ index.php
@@ -51,7 +51,59 @@
  </head>
 
 <style>
+body {
+  background-color:  #eee;
+}
+.title {
+ 
+    margin-bottom: 50px;
+    text-transform: uppercase;
+}
 
+.card-block {
+    font-size: 1em;
+    position: relative;
+    margin: 0;
+    padding: 1em;
+    border: none;
+    border-top: 1px solid rgba(34, 36, 38, .1);
+    box-shadow: none;
+     
+}
+.card {
+    font-size: 1em;
+    overflow: hidden;
+    padding: 5;
+    border: none;
+    border-radius: .28571429rem;
+    box-shadow: 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5;
+    margin-top:20px;
+}
+
+.carousel-indicators li {
+    border-radius: 12px;
+    width: 12px;
+    height: 12px;
+    background-color: #404040;
+}
+.carousel-indicators li {
+    border-radius: 12px;
+    width: 12px;
+    height: 12px;
+    background-color: #404040;
+}
+.carousel-indicators .active {
+    background-color: white;
+    max-width: 12px;
+    margin: 0 3px;
+    height: 12px;
+}
+
+
+
+.btn {
+  margin-top: auto;
+}
 </style>
 
  <body class="host_version">  
@@ -217,8 +269,70 @@
 		 </div><!-- end container -->
 	 </div><!-- end section -->
 
+ <!-- HOLIDAY SCHEDULE -->
+ <div id="plan" class="section lb">
+         <div class="container">
+             <div class="section-title text-center">
+                 <h3>School principals</h3>
+                 <p>School principals are the link between teachers, students and their parents or guardians, the education system and the wider community.  </p>
+             
+  <!-- Card Start -->
+  <div class="card">
+    <div class="row ">
 
-     
+      <div class="col-md-7 px-3">
+        <div class="card-block px-6">
+          <h1 class="card-title text-left"><b>Teaching staff</b> </h1>
+          <p class="card-text text-left">
+      Teaching staff refer to professional personnel directly involved in teaching students, including classroom teachers; special education teachers; and other teachers who work with students as a whole class in a classroom, 
+            in small groups in a resource room, or in one-to-one teaching inside or outside a regular classroom.</p>
+          <br>
+          <a href="#" class="mt-auto btn btn-warning">Read More</a>
+        </div>
+      </div>
+      <!-- Carousel start -->
+      <div class="col-md-5">
+        <div id="CarouselTest" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#CarouselTest" data-slide-to="0" class="active"></li>
+            <li data-target="#CarouselTest" data-slide-to="1"></li>
+            <li data-target="#CarouselTest" data-slide-to="2"></li>
+
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block" src="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/9/1/1441131168821/b3d3ceba-4b86-4ff7-b9f0-a1e913f8f5f0-2060x1236.jpeg?width=620&quality=85&fit=max&s=e0a0a71127de6373af8752015d6f96d2" width="600px" height="350px" alt="">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block" src="https://www.gannett-cdn.com/-mm-/84de89d94c58e3446288642c313aba4eab6b8f24/c=0-102-2000-1232/local/-/media/2015/11/19/WIGroup/FondduLac/635835504969164934-FON-111915-nfdl-4.jpg?width=2000&height=1130&fit=crop&format=pjpg&auto=webp" width="600px" height="350px" alt="">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block" src="https://www.payscale.com/wp-content/uploads/2021/10/teacher.jpg" width="600px" height="350px" alt="">
+            </div>
+            <a class="carousel-control-prev" href="#CarouselTest" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+            <a class="carousel-control-next" href="#CarouselTest" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+          </div>
+        </div>
+      </div>
+      <!-- End of carousel -->
+    </div>
+  </div>
+  <!-- End of card -->
+  </div><!-- end title -->
+</div>
+
+
+ 
+ <br>
+<br>
+ 
+
 
 
 
