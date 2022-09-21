1e7c98f94a5dddbd0501d461551da557359bdc33
Momna Khan <87558364+MomnaKhan@users.noreply.github.com>
2022-09-20 22:07:20 -0700

    a

--- index.php
+++ index.php
@@ -50,61 +50,57 @@
      <![endif]-->
  </head>
 
-<style>
-body {
-  background-color:  #eee;
-}
-.title {
+ <style>
+     body {
+      background-color:  #eee;
+        }
+     .title {
  
-    margin-bottom: 50px;
-    text-transform: uppercase;
-}
+     margin-bottom: 50px;
+     text-transform: uppercase;
+     }
 
-.card-block {
-    font-size: 1em;
-    position: relative;
-    margin: 0;
-    padding: 1em;
-    border: none;
-    border-top: 1px solid rgba(34, 36, 38, .1);
-    box-shadow: none;
+     .card-block {
+     font-size: 1em;
+     position: relative;
+     margin: 0;
+     padding: 1em;
+     border: none;
+     border-top: 1px solid rgba(34, 36, 38, .1);
+     box-shadow: none;
      
-}
-.card {
-    font-size: 1em;
-    overflow: hidden;
-    padding: 5;
-    border: none;
-    border-radius: .28571429rem;
-    box-shadow: 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5;
-    margin-top:20px;
-}
-
-.carousel-indicators li {
-    border-radius: 12px;
-    width: 12px;
-    height: 12px;
-    background-color: #404040;
-}
-.carousel-indicators li {
-    border-radius: 12px;
-    width: 12px;
-    height: 12px;
-    background-color: #404040;
-}
-.carousel-indicators .active {
-    background-color: white;
-    max-width: 12px;
-    margin: 0 3px;
-    height: 12px;
-}
-
-
-
-.btn {
-  margin-top: auto;
-}
-</style>
+     }
+     .card {
+     font-size: 1em;
+     overflow: hidden;
+     padding: 5;
+     border: none;
+     border-radius: .28571429rem;
+     box-shadow: 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5;
+     margin-top:20px;
+     }
+     .carousel-indicators li {
+     border-radius: 12px;
+     width: 12px;
+     height: 12px;
+     background-color: #404040;
+     }
+     .carousel-indicators li {
+     border-radius: 12px;
+     width: 12px;
+     height: 12px;
+     background-color: #404040;
+     }
+     .carousel-indicators .active {
+     background-color: white;
+     max-width: 12px;
+     margin: 0 3px;
+     height: 12px;
+     }
+     .btn {
+     margin-top: auto;
+     }
+ </style>
 
  <body class="host_version">  
      <?php
@@ -203,11 +199,7 @@ body {
 		 </div>
 	 </div>
 	 <!-- Slider End -->
-
-
-
-
-     
+  
     
 	 <div class="section cl">
 	     <div class="container">
@@ -269,10 +261,7 @@ body {
 		 </div><!-- end container -->
 	 </div><!-- end section -->
 
-<<<<<<< HEAD
 
-=======
->>>>>>> a2e24948b56d1a324159f0885799ae1be1225371
  <!-- HOLIDAY SCHEDULE -->
  <div id="plan" class="section lb">
          <div class="container">
@@ -336,13 +325,15 @@ body {
  <br>
 <br>
  
-
-
-
 <<<<<<< HEAD
-=======
+<<<<<<< HEAD
 
->>>>>>> a2e24948b56d1a324159f0885799ae1be1225371
+
+
+=======
+>>>>>>> 7051e8d4f071d026aa816ea7b78782a23e2b0738
+=======
+>>>>>>> 7051e8d4f071d026aa816ea7b78782a23e2b0738
      <br>
      <div class="container mt-2">
 <!--   <div class="card card-block mb-2">
@@ -387,7 +378,6 @@ body {
   
 </div>
 
-<<<<<<< HEAD
 <div id="overviews" class="section lb">
          <div class="container">
              <div class="section-title row text-center">
@@ -482,8 +472,6 @@ There is a vast collection of books. There are around 15,000 books in my school 
 
 
      
-=======
->>>>>>> a2e24948b56d1a324159f0885799ae1be1225371
 
      <!-- HOLIDAY SCHEDULE -->
      <div id="plan" class="section lb" style="background-image:url('https://chytomo.com/wp-content/uploads/2022/03/education-day-assortment-with-copy-space.jpg')" width="100px">
--- signup.php
+++ signup.php
@@ -3,13 +3,13 @@
 
  <head>
      <meta charset="utf-8">
-     <title>Sign Up</title>
+     <title>Online Education</title>
      <meta content="width=device-width, initial-scale=1.0" name="viewport">
      <meta content="" name="keywords">
      <meta content="" name="description">
 
-     <!-- Favicon -->
-     <link href="img/logo.png" rel="icon">
+     <!--logo--->
+     <link rel="icon" type="image" href="images/mimilogo.png">
 
      <!-- Google Web Fonts -->
      <link rel="preconnect" href="https://fonts.googleapis.com">
